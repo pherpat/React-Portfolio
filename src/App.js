c903fc69fb950fd161f953c948bbf43d5c83220f
JENNIFER SCHERSCHLIGT <patinopher@gmail.com>
2023-03-25 18:45:45 -0700

    Updated about, contact, Resume and portfolio files.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-import About from "./components/About.js";
-import Contact from "./components/Contact";
-import Experience from "./components/Experience";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Portfolio from "./components/Portfolio";
-
-function App() {
-  return (
-    <div>
-       <About />
-       <Contact />
-       <Experience />
-       <Footer />
-       <Header />
-       <Navbar />
-       <Portfolio />
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
