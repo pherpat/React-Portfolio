8a6d227f736cc7075a89309179cd17e37c6dea69
JENNIFER SCHERSCHLIGT <patinopher@gmail.com>
2023-03-27 11:08:02 -0700

    Updated pages, added pointer to footer.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,14 +10,14 @@ function About() {
       <div className='aboutHero'>
         <div className='containerMain'>
           <div className='aboutLine'></div>
-          <h1 className='aboutHeader'>Hi! 😊 I’m Jen, and i’m learning to be a developer.</h1>
+          <h1 className='aboutHeader'>Hi! 😊 I’m Jen, and I’m learning to be a developer.</h1>
           <div className='toots'><img src={toots} alt="Toots" width='100%'></img></div>
         </div>
       </div>
       <div className='aboutInfo'>
         <div className='containerMain aboutFlex'>
           <div className='jenn'><img src={jenPic} alt="Jenn" width="100%"></img></div>
-          <div className='aboutText'>Learning to code and taking care of kids is a full time fun! Welcome to my portfolio of projects i created during my Full Stack Bootcamp at the University of Oregon in the rainy PNW.</div>
+          <div className='aboutText'>Learning to code and taking care of kids is a full time fun! Welcome to my portfolio of projects I created during my Full Stack Bootcamp at the University of Oregon in the rainy PNW.</div>
         </div>
       </div>
     </div>
