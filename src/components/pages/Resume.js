550a02a9de451b8dbcc24b9d735861eb69b01d4d
JENNIFER SCHERSCHLIGT <patinopher@gmail.com>
2025-09-02 13:18:12 -0400

    Update resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,14 +1,17 @@
 // First we import components from our components folder
 import React from 'react';
+import resume from '../assets/jen-resume.jpg';
 
 export default function Resume() {
   return (
     
     <div className='containerMain'>
-      <h1>Resume</h1>
-      <p>
-        Coming soon!
-      </p>
+      <h1 className="portfolioHeader">Resume</h1>
+     <div className="resume">
+
+     <img src={resume} width="100%" alt="resume" ></img> 
+
+     </div>
     </div>
   );
 }
